b00354906586d5541c78a5abfae4ebe139c9c5dd
prashant kumar <prashantmanwan@gmail.com>
2022-06-01 21:01:20 +0530

    First Commit - Operators

---
+++ 1 June/operator3.js
@@ -0,0 +1,7 @@
+function result() {
+    var a = parseInt(document.getElementById("no1").value);
+    var b = parseInt(document.getElementById("no2").value);
+
+    document.getElementById("result").value = (a + b);
+
+}
